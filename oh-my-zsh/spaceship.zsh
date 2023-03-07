29b88b8d43cce499ec5d66826bf46b3b4d8c5464
Sean Su <sean.su@unity3d.com>
2023-03-07 11:15:25 -0800

    update docker context

--- oh-my-zsh/spaceship.zsh
+++ oh-my-zsh/spaceship.zsh
@@ -8,3 +8,11 @@ SPACESHIP_GCLOUD_SHOW=false
 SPACESHIP_KUBECTL_SHOW=true
 SPACESHIP_KUBECTL_VERSION_SHOW=false
 SPACESHIP_KUBECTL_SYMBOL='☸️  '
+
+# Setting prefix and suffix for docker_context section
+SPACESHIP_DOCKER_CONTEXT_PREFIX="on "
+SPACESHIP_DOCKER_CONTEXT_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+
+# Replacing docker with docker_context
+spaceship remove docker
+spaceship add docker_context
