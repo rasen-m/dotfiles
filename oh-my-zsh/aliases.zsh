8cb7d1ad4f5f0a5c7a964d4b071f5e278019ab2e
Sean Su <seansu@me.com>
2023-05-02 08:36:22 -0700

    update.

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -1,4 +1,7 @@
-# Commonly accessed folders
+# Commonly accessed folders.
 alias .d='cd ~/Downloads/'
 alias .r='cd ~/repo/'
 alias .w='cd ~/repo/capture'
+
+# Commonly used commands.
+alias python='python3'
