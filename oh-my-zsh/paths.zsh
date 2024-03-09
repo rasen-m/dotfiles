45a82a9d45e2f57f1eefb8c72841575e7f9011a1
Sean Su <seanisu@amazon.com>
2024-03-08 20:22:08 -0800

    update.

--- oh-my-zsh/paths.zsh
+++ oh-my-zsh/paths.zsh
@@ -1,4 +1,9 @@
 export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/.toolbox/bin:$PATH
+
+export LDFLAGS="-L/opt/homebrew/opt/libffi/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/libffi/include"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/libffi/lib/pkgconfig"
 
 # Android paths. ADB.
 # export ANDROID_HOME=/Users/$USER/Library/Android/sdk
